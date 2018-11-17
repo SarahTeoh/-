51d31af6a57e7b94ad6bb032549006bc07b803fd
Sarahサラ <sarah.teoh752@gmail.com>
2018-11-17 15:10:34 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -4,23 +4,25 @@ from tkinter import *
 from tkinter import ttk
 import time
 
+PRACTICETIME=120
+
 class App(tk.Tk):   
     def __init__(self, *args, **kwargs):  
         tk.Tk.__init__(self, *args, **kwargs)
         
         tk.Tk.wm_title(self,"PreSys")
 
-        container = tk.Frame(self, height = 1000, width =1000)
-        container.pack(side="top", fill="both", expand = True)
+        self.container = tk.Frame(self, height = 1000, width =1000)
+        self.container.pack(side="top", fill="both", expand = True)
 
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (SignInPage, StartPage, PracticePage):
+        for F in (SignInPage, StartPage):
 
-            frame = F(container, self)
+            frame = F(self.container, self)
 
             self.frames[F] = frame
 
@@ -30,9 +32,21 @@ class App(tk.Tk):
 
     def show_frame(self, cont):
         frame = self.frames[cont]
-        frame.tkraise()       
+        """if cont == PracticePage:
+            frame.remaining = PRACTICETIME
+            frame.setTimeLeft()"""
+        frame.tkraise()   
 
-        
+    def create_practicePage(self,cont):
+        frame = PracticePage(self.container, self)
+
+        self.frames[PracticePage] = frame
+
+        frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(PracticePage)
+
+
 class SignInPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self,parent)
@@ -52,7 +66,7 @@ class StartPage(tk.Frame):
 
 
         self.label = tk.Label(self,text = "スタートボタンを押してプレゼンテーションを始めてください")
-        self.startButton = ttk.Button(self, text="スタート", command = lambda: controller.show_frame(PracticePage))
+        self.startButton = ttk.Button(self, text="スタート", command = lambda: controller.create_practicePage(PracticePage))
 
         self.label.pack()
         self.startButton.pack()
@@ -68,9 +82,8 @@ class PracticePage(tk.Frame):
         self.backButton.pack()
         self.homeButton.pack()
         
-        
         self.remaining = 0
-        self.countdown(122)
+        self.countdown(120)
 
     def countdown(self, remaining = None):
         if remaining is not None:
@@ -85,7 +98,6 @@ class PracticePage(tk.Frame):
             self.timeLeft.configure(text=str(mins) +"分"+ str(secs) +"秒")
             self.remaining = self.remaining - 1
             self.after(1000, self.countdown)
-
 
 apps = App()
 apps.mainloop()
