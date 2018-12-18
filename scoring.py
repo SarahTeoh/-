843f330c7cf7cdf517a79085b103ad2f7f4bd684
Sarahサラ <sarah.teoh752@gmail.com>
2018-12-18 17:01:39 +0900

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -6,7 +6,7 @@ def cos_sim(speechrate, pitch):
     v1 = np.array([300, 30])     #理想な値の行列[話速度(字/秒), ピッチの標準偏差(Hz)]
     v2 = np.array([speechrate, pitch])     #ユーザの話速度とピッチの行列
     similarity = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)) #0から1の値
-    #print("{:.1f}".format(similarity*100))
+    print("{:.1f}".format(similarity*100))
     return "{:.1f}".format(similarity*100)
     
 
@@ -25,7 +25,7 @@ def speechRateFinalComment(speechrate):
     elif 200<= speechrate < 250:
         rate_comment = "もう少し速く話しましょう!"
     else:
-        rate_comment = "この話スピードだと聴衆は寝ちゃいます!"
+        rate_comment = "この話スピードだと聴衆は寝ちゃいますよ!"
     return rate_comment    
 
 def pitchComment(hensa):
